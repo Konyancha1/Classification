d0d6ec2f32fe48084b9cae993b4beebc88346b3c
Konyancha1 <kmutua791@gmail.com>
2023-11-29 01:11:19 +0200

    Files modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import torch
-from torchvision import transforms
+import requests
 from PIL import Image
 import numpy as np
 
 # Function to load the PyTorch model
 @st.cache(allow_output_mutation=True)
-def load_model(model_path):
+def load_model_from_url(url):
+    response = requests.get(url)
+    model_path = '/tmp/my_model.pth'
+    with open(model_path, 'wb') as f:
+        f.write(response.content)
     model = torch.load(model_path, map_location=torch.device('cpu'))
     return model
 
@@ -26,9 +30,11 @@ threshold = 0.5
 # Streamlit UI
 st.title('Image Classification')
 
-# Load the PyTorch model
-model_path = 'my_model.pth'
-model = load_model(model_path)
+# URL to your model file
+model_url = 'https://drive.google.com/file/d/1fZtoOlxfqT1J7AED4aS6z_NgpFUe-rtX/view?usp=sharing'
+
+# Load the PyTorch model from the URL
+model = load_model_from_url(model_url)
 
 uploaded_file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png', 'bmp'])
 
